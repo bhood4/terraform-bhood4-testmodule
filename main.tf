1ad930ffa10909a8210424eddea46c6db8c8b316
Bill Hood <bhood4@gmail.com>
2020-06-30 10:27:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ provider "google" { }
 data "google_compute_network" "my_network" {
   name = "default"
 }
+data "google_compute_subnetwork" "my_subnetwork" {
+  name   = "default"
+}
 
 resource "google_compute_instance" "vm_instance" {
   name         = var.name
@@ -14,8 +17,7 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
   network_interface {
-    # A default network is created for all GCP projects
-    network       = data.google_compute_network.my_network.self_link
+    subnetwork = data.google_compute_subnetwork.my_subnetwork.self_link
     access_config {
     }
   }

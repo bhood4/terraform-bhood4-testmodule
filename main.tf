de88d5f13eae8f64e008787ef78e3207fc586171
Bill Hood <bhood4@gmail.com>
2020-06-30 10:00:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "google" { }
 
+data "google_compute_network" "my_network" {
+  name = "default"
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = var.name
   machine_type = "n1-standard-2"
@@ -7,6 +11,12 @@ resource "google_compute_instance" "vm_instance" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = data.google_compute_network.my_network.self_link
+    access_config {
     }
   }
 }

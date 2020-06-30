d34a3cefcd5336fd2bcae30e9afb4fef60e194c9
Bill Hood <bhood4@gmail.com>
2020-06-29 20:23:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" { }
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = var.name
 
   boot_disk {
     initialize_params {
